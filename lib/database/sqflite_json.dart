cb7fe0bf4d73b550dfb8374232f21f3717399cab
o-ifeanyi <onuifeanyi95@gmail.com>
2023-08-13 22:03:38 +0100

    clean up

--- lib/database/sqflite_json.dart
+++ lib/database/sqflite_json.dart
@@ -5,7 +5,6 @@ import 'package:db_benchmarks/interface/benchmark.dart';
 import 'package:db_benchmarks/interface/user.dart';
 import 'package:db_benchmarks/model/user.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart' as path;
 
